5ffaedda9b0df77b07ffe9f5cdd6e3120aa9f2c4
Mazurier <antoinemazuriermail@gmail.com>
2023-05-04 14:25:47 +0200

    Commun & debut pages article

--- commun.js
+++ commun.js
@@ -1,9 +1,11 @@
+// ../path/to/file pour que ca marche aussi avec les page d'articles
+
 var headervar='';
 
 function header(){
 
 return `
-<img src="img/bannerindex.png" class="headerimg">
+<img src="../img/bannerindex.png" class="headerimg">
 `
 
 }
@@ -19,21 +21,21 @@ function navbarjs(){
 
 return `
 <nav class="navbar">
-        <a href="#" class="logo"><img src="./img/logo-ducsman-200-95.png"></a>
+        <a href="#" class="logo"><img src="../img/logo-ducsman-200-95.png"></a>
         <div class="nav-links">
-            <img src="./img/menu/menu-btn.png" alt="bouton menu hamburger" class="menu-hamburger" id="menu-hamburger-image">
-            <a href="#" class="logo-mobile"><img src="./img/logo-ducsman-200-95.png"></a>
+            <img src="../img/menu/menu-btn.png" alt="bouton menu hamburger" class="menu-hamburger" id="menu-hamburger-image">
+            <a href="#" class="logo-mobile"><img src="../img/logo-ducsman-200-95.png"></a>
             <ul>
-                <li class="active"><a href="./index.html">Boutique</a></li>
-                <li><a href="./apropos.html">à propos</a></li>
-                <li><a href="./contact.html">Contact</a></li>
-                <li><a href="./sav.html">SAV</a></li>
+                <li class="active"><a href="../index.html">Boutique</a></li>
+                <li><a href="../apropos.html">à propos</a></li>
+                <li><a href="../contact.html">Contact</a></li>
+                <li><a href="../sav.html">SAV</a></li>
             </ul>
             <iframe id="small-google-map" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d10498.368656278535!2d2.3234495763383443!3d48.86598692289923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1stailleur%20paris!5e0!3m2!1sfr!2sfr!4v1683188216931!5m2!1sfr!2sfr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             <div id="social-network">
-                <a href="https://www.instagram.com"><img class="social-network-logo" src="./img/menu/logo-instagram.png"></a>
-                <a href="https://www.facebook.com"><img class="social-network-logo" src="./img/menu/logo-facebook.png"></a>
-                <a href="https://www.twitter.com"><img class="social-network-logo" src="./img/menu/logo-twitter.png"></a>    
+                <a href="https://www.instagram.com"><img class="social-network-logo" src="../img/menu/logo-instagram.png"></a>
+                <a href="https://www.facebook.com"><img class="social-network-logo" src="../img/menu/logo-facebook.png"></a>
+                <a href="https://www.twitter.com"><img class="social-network-logo" src="../img/menu/logo-twitter.png"></a>    
             </div>
         </div>
     </nav>
