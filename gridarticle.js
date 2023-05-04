0380446d89f991a8dbb49e28320347228e206484
Mazurier <antoinemazuriermail@gmail.com>
2023-05-04 11:07:16 +0200

    Multiple article JS

--- gridarticle.js
+++ gridarticle.js
@@ -1,25 +1,25 @@
 var gridarticle='';
 
-function gridaddtxt(){
+function gridaddtxt(title,img,prix,tag1,tag2,tag3){
 
 return `
 <div class="gridarticle">
     <div class="gridprix">
-        30€
+        ${prix}€
     </div>
-    <img src="img/gridarticle.png" class="gridimage">
+    <img src="img/${img}" class="gridimage">
     <div class="gridtitle">
-        Le tshirt topbien
+        ${title}
     </div>
     <div class="gridtagplace">
         <div class="gridtag">
-            test
+            ${tag1}
         </div>
         <div class="gridtag">
-            trucmuchechouette
+            ${tag2}
         </div>
         <div class="gridtag">
-            blabla
+            ${tag3}
         </div>
     </div>
 </div>
@@ -29,8 +29,9 @@ return `
 
 
 //Copié collé une ligne pour ajouter un article
-//Nom de l'article, prix, tag1, tag2, tag3, (si vide "")
-gridarticle = gridarticle + gridaddtxt("Le tshirt tropbien", 'gridarticle.png', "test","trucmuchouette","blabla");
-gridarticle = gridarticle + gridaddtxt("Le tshirt tropbien", 'gridarticle.png', "test","trucmuchouette","blabla");
+//Nom de l'article, image de l'article, prix, tag1, tag2, tag3, (si vide "")
+gridarticle = gridarticle + gridaddtxt("Le tshirt tropbien", 'gridarticle.png', "30", "test","trucmuchouette","blabla");
+gridarticle = gridarticle + gridaddtxt("Le tshirt superbeau", 'gridarticle2.png',"2,99","pascher","plastique","");
+gridarticle = gridarticle + gridaddtxt("Ma chemise d'hier", 'gridarticle3.png',"399.64","sale","pas repasser","sent l'ognion");
 
 document.getElementById("grid").innerHTML=gridarticle;
