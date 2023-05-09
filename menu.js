c935feff34a63c4f3e648b558675bcc594bfc928
David De Castro <segphault@gmail.com>
2023-05-09 10:54:55 +0200

    Correction menu

--- menu.js
+++ menu.js
@@ -9,18 +9,29 @@ const hamburgerImg = document.getElementById('menu-hamburger-image');
 const navbar = document.getElementById('navbar');
 const navLinksId = document.getElementById('nav-links');
 
+let isMenuOpened=false;
 console.log(navbar);
 
 menuHamburger.addEventListener('click',()=>{
+    navLinks.classList.toggle('mobile-menu');
     img=hamburgerImg.getAttribute('src');
     if (img=='./img/menu/menu-btn.png') {
+        isMenuOpened=true;
         hamburgerImg.setAttribute('src','./img/menu/close.png');
         navbar.style.height = '100vh';
         navLinksId.style.height = '100vh';
     } else {
+        isMenuOpened=false;
         hamburgerImg.setAttribute('src','./img/menu/menu-btn.png');  
-        navbar.style.height = '35px';
-        navLinksId.style.height = '35px';
+        navLinks.ontransitionend= () => {
+            if (!isMenuOpened) {
+                navbar.style.height = '35px';
+                navLinksId.style.height = '35px';
+            }
+        }
     }
-    navLinks.classList.toggle('mobile-menu');
 });
+
+function test() {
+    return;
+}
