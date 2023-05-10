1e72539c1f13fa947c2cd839be67d90e15f6dc4e
william.biencourt@live.fr <william.biencourt@live.fr>
2023-05-10 10:51:03 +0200

    update_william

--- gridarticle.js
+++ gridarticle.js
@@ -36,8 +36,8 @@ function gridaddtxt(title, img, prix, lien, tag1, tag2, tag3) {
   gridarticle += gridaddtxt("Pantalon Morgana", 'gridarticle4.jpg', "90", "Coton", "Disponible en différents coloris", "Prêt-à-porter", "");
   gridarticle += gridaddtxt("Smoking Lucie", 'gridarticle5.jpg', "À partir de 500", "Laine", "Soie", "Sur mesure uniquement", "");
   gridarticle += gridaddtxt("Veste Aika", 'gridarticle6.jpg', "120", "article1", "Coton", "Prêt-à-porter", "Disponible en différents coloris");
-  gridarticle += gridaddtxt("Chemise Chiaki", 'gridarticle7.jpg', "60", "article1", "Coton", "Prêt-à-porter", "");
-  gridarticle += gridaddtxt("Chemisette Zoé", 'gridarticle8.jpg', "50", "article1", "Coton", "Prêt-à-porter", "");
+  gridarticle += gridaddtxt("Chemise Chiaki", 'gridarticle7.jpeg', "60", "article1", "Coton", "Prêt-à-porter", "");
+  gridarticle += gridaddtxt("Chemisette Zoé", 'gridarticle8.jpeg', "50", "article1", "Coton", "Prêt-à-porter", "");
   gridarticle += gridaddtxt("Borsalino Kana", 'gridarticle9.jpg', "50", "article1", "Feutre", "Prêt-à-porter", "");
   gridarticle += gridaddtxt("Cravate Denise", 'gridarticle10.jpg', "60", "article1", "Coton", "Disponible en différents motifs et coloris", "");
   gridarticle += gridaddtxt("Boutons de manchette Megumi", 'gridarticle11.jpg', "150", "article1", "Argent", "", "");
